8993d5d46b90c560e129b152395ea8c11b469e6e
figoathallah <figoirawan02@gmail.com>
2023-05-15 00:46:14 +0700

    Implement orchestration mechanism with localhost urls

--- orchester/urls.py
+++ orchester/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('api/payment/create-payment', views.payment_service),
+    path('api/convert/pdf-to-img', views.convert_service),
+    path('', views.service_orchester),
 ]
--- orchester/views.py
+++ orchester/views.py
@@ -2,37 +2,55 @@ from django.http import JsonResponse
 import requests
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .serializers import *
+
+#TODO: 
+# Explore Consul for service registry (low priority, refactor after orchestration works)
+
  
-def paymentService(APIView):
-    def post(self, request):
-        serializer = PembayaranSerializer(data=request.data)
-        if serializer.is_valid():
+@api_view(['POST'])
+def payment_service(request):
+    serializer = PembayaranSerializer(data=request.data)
+    if serializer.is_valid():
 
-            pembayaran = Pembayaran.objects.filter(username=serializer.validated_data['username']).first()
-            if pembayaran is not None:
-                return Response({"message": "You already make payment"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            pembayaran = serializer.save()
-            detail_serializer = DetailPembayaranSerializer(instance=pembayaran)
-            return Response(detail_serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        pembayaran = Pembayaran.objects.filter(username=serializer.validated_data['username']).first()
+        if pembayaran is not None:
+            return Response({"message": "You already make payment"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        pembayaran = serializer.save()
+        detail_serializer = DetailPembayaranSerializer(instance=pembayaran)
+        return Response(detail_serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-def convertService(request):
-    return JsonResponse({'status': 'success'})
-
-def serviceOrchester(request):
+@api_view(['POST'])
+def convert_service(request, username):
     fileInput = request.FILES.get('fileInput')
-    imageFormat = request.POST.get('imageFormat')
-    singleOrMultiple = request.POST.get('singleOrMultiple')
-    colorType = request.POST.get('colorType')
-    dpi = request.POST.get('dpi')
-    username = request.POST.get('username')
+    imageFormat = request.data.get('imageFormat')
+    singleOrMultiple = request.data.get('singleOrMultiple')
+    colorType = request.data.get('colorType')
+    dpi = request.data.get('dpi')
 
-    paymentResponse = requests.post('/api/payment/upgrade-membership',
+    convert_url = 'http://localhost:8080/api/convert/pdf-to-img'
+    files = {'fileInput': fileInput}
+    data = {'imageFormat': imageFormat,
+            'singleOrMultiple': singleOrMultiple,
+            'colorType': colorType,
+            'dpi': dpi,
+            'username': username,}
+    convert_response = requests.post(convert_url, files=files, data=data)
+
+    if convert_response.status_code != 200:
+        return JsonResponse({'error': 'Conversion process failed!'})
+    
+    return Response({'data': convert_response.content})
+
+@api_view(['POST'])
+def service_orchester(request):
+    username = request.data.get('username')
+
+    paymentResponse = requests.post('http://127.0.0.1:8000/api/payment/create-payment',
         data = {'username': username})
 
     if paymentResponse.status_code != 200:
@@ -40,18 +58,9 @@ def serviceOrchester(request):
     elif paymentResponse.status_code == 400:
         return JsonResponse({'message': 'You are already a Premium user of ilovelaw'})
     
-    convertResponse = requests.post('api/convert/pdf-to-img', 
-        data={'fileInput': fileInput,
-              'imageFormat': imageFormat,
-              'singleOrMultiple': singleOrMultiple,
-              'colorType': colorType,
-              'dpi': dpi
-              })
-    
-    if convertResponse.status_code != 200:
-        return JsonResponse({'error': 'Conversion process failed!'})
-    
-    return JsonResponse({'data': convertResponse.content})
+    convertResponse = convert_service(request._request, username)
+
+    return convertResponse
 
 
 

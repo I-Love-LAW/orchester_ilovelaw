84e77e29b427e4b5a2c9420b761fea0ab7419b48
Dimas Ichsanul Arifin <dimas.ichsanul@ui.ac.id>
2023-05-21 15:35:56 +0700

    Refactor for best practice

--- orchester_ilovelaw/urls.py
+++ orchester_ilovelaw/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/orchester', include('orchester.urls'))
+    path('api/orchester/', include('orchester.urls'))
 ]
